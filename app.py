0bdca80be4276698d445b3cf5fdf03cf17a363da
Aleksandrs Barbins <bbarbins@gmail.com>
2021-11-05 00:32:06 +0200

    Comment Scrape and DB Creation

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from log import get_logger
-from postRead import readPost
 from postRead import flipPages
+from postRead import commentRead
 from bs4 import BeautifulSoup
 import requests
 
 logger = get_logger(__name__)
 
-pages = 3
+pages = 1
 
 
 # readPost()
 
 flipPages(pages)
 
-# readComm()
+# commentRead(str(53740237))
